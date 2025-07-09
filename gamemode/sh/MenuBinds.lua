7ce73f0243acb72f33109ace65df68b4502e86bb
delexyay76 <pisunkapiska@mail.ru>
2025-07-10 01:59:00 +0300

    Delete gamemode directory

--- gamemode/sh/MenuBinds.lua
+++
@@ -1,5 +0,0 @@
-hook.Add("PlayerButtonDown", "CourseMenuBind", function(ply, button)
-	if (game.SinglePlayer() or CLIENT and IsFirstTimePredicted()) and button == KEY_F4 then
-		ply:ConCommand("Beatrun_CourseMenu")
-	end
-end)
